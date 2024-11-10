786c10bed77c7aaaaaf63fefc7d54e235378e4ca
sst-smart <79058136+sst-smart@users.noreply.github.com>
2024-11-10 06:07:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function signUp() {
         return;
     }
 
-    users.push({ name, email, password });
+    users.push({name, email, password});
     localStorage.setItem('users', JSON.stringify(users));
     alert('Sign-up successful! You can now log in.');
 
@@ -27,8 +27,8 @@ function signUp() {
     document.getElementById('signUpPassword').value = '';
 }
 
-document.getElementById('cryptoTableHead').style.visibility = 'hidden';
 document.getElementById('control').style.visibility = 'hidden';
+document.getElementById('cryptoTable').style.visibility = 'hidden';
 
 // Login Function
 function login() {
@@ -43,16 +43,15 @@ function login() {
     // const users = JSON.parse(localStorage.getItem('users') || '[]');
 
     const users = [
-        {name:'Mohamed', email:'admin', password:'321'},
+        {name: 'Mohamed', email: 'admin', password: '321'},
     ]
-    console.log(users);
     const user = users.find(user => user.email === email && user.password === password);
 
     if (user) {
         // alert('Login successful! Welcome, ' + user.name + '.');
         setInterval(getPrice, 2000);
         document.getElementById('loginPage').style.display = 'none';
-        document.getElementById('cryptoTableHead').style.visibility = 'visible';
+        document.getElementById('cryptoTable').style.visibility = 'visible';
         document.getElementById('control').style.visibility = 'visible';
         // Redirect or perform further actions here
     } else {
@@ -67,77 +66,96 @@ function login() {
 
 // Send API request and fetch data
 const apiUrl = 'https://api.binance.com/api/v3/ticker/24hr';
+
 async function getPrice() {
     const response = await fetch(apiUrl);
     const data = await response.json();
     const tableBody = document.getElementById('cryptoTableBody');
     tableBody.innerHTML = '';
     data.forEach((item) => {
-        if (item["symbol"].endsWith('USDT') && eval(item["lastPrice"]) !== 0 && eval(item["priceChangePercent"]) > document.getElementById('percentage').value) {
-            const row = document.createElement('tr');
+        if (
+            item["symbol"].endsWith('USDT') &&
+            eval(item["lastPrice"]) !== 0 &&
+            eval(item["priceChangePercent"]) >= document.getElementById('percentage').value &&
+            eval(item["lastPrice"]) >= document.getElementById('currencyPrice').value
+        ) {
+            if (document.getElementById('currency').value) {
+                if (item["symbol"] === document.getElementById('currency').value){
+                    if (soundAlarm) {
+                        new Audio("/Website-JavaScript/project_04_binance/airport-announcement-ding.wav").play();
+                    }
+                    const row = document.createElement('tr');
+                    const symbol = document.createElement('td');
+                    symbol.textContent = item["symbol"];
+                    row.appendChild(symbol);
+                    const priceChangePercent = document.createElement('td');
+                    priceChangePercent.textContent = eval(item["priceChangePercent"]) + ' %';
+                    row.appendChild(priceChangePercent);
+                    const lastPrice = document.createElement('td');
+                    lastPrice.textContent = Number(item["lastPrice"]).toLocaleString(undefined, {minimumFractionDigits: 8});
+                    row.appendChild(lastPrice);
+                    // const volume = document.createElement('td');
+                    // volume.textContent = item["volume"];
+                    // row.appendChild(volume);
+                    // const openTime = document.createElement('td');
+                    // let openDate = new Date(Number(item["openTime"]));
+                    // openTime.textContent = openDate.toLocaleString();
+                    // row.appendChild(openTime);
+                    // const closeTime = document.createElement('td');
+                    // let closeDate = new Date(Number(item["closeTime"]));
+                    // closeTime.textContent = closeDate.toLocaleString();
+                    // row.appendChild(closeTime);
+                    // Append row to the table body
+                    tableBody.appendChild(row);
+                }
 
-            const symbol = document.createElement('td');
-            symbol.textContent = item["symbol"];
-            row.appendChild(symbol);
-            const priceChangePercent = document.createElement('td');
-            priceChangePercent.textContent = eval(item["priceChangePercent"]) + ' %';
-            if (eval(item["priceChangePercent"]) < 0) {
-                priceChangePercent.style.color = 'red';
-            } else if(eval(item["priceChangePercent"]) > document.getElementById('percentage').value) {
+            } else {
                 if (soundAlarm) {
                     new Audio("/Website-JavaScript/project_04_binance/airport-announcement-ding.wav").play();
                 }
-                symbol.style.backgroundColor = 'green';
-            } else {
-                priceChangePercent.style.color = 'blue';
+                const row = document.createElement('tr');
+                const symbol = document.createElement('td');
+                symbol.textContent = item["symbol"];
+                row.appendChild(symbol);
+                const priceChangePercent = document.createElement('td');
+                priceChangePercent.textContent = eval(item["priceChangePercent"]) + ' %';
+                row.appendChild(priceChangePercent);
+                const lastPrice = document.createElement('td');
+                lastPrice.textContent = Number(item["lastPrice"]).toLocaleString(undefined, {minimumFractionDigits: 8});
+                row.appendChild(lastPrice);
+                // const volume = document.createElement('td');
+                // volume.textContent = item["volume"];
+                // row.appendChild(volume);
+                // const openTime = document.createElement('td');
+                // let openDate = new Date(Number(item["openTime"]));
+                // openTime.textContent = openDate.toLocaleString();
+                // row.appendChild(openTime);
+                // const closeTime = document.createElement('td');
+                // let closeDate = new Date(Number(item["closeTime"]));
+                // closeTime.textContent = closeDate.toLocaleString();
+                // row.appendChild(closeTime);
+                // Append row to the table body
+                tableBody.appendChild(row);
+
             }
-            row.appendChild(priceChangePercent);
-            const lastPrice = document.createElement('td');
-            lastPrice.textContent = Number(item["lastPrice"]).toLocaleString(undefined, {minimumFractionDigits: 8});
-            row.appendChild(lastPrice);
-            const openPrice = document.createElement('td');
-            openPrice.textContent = item["openPrice"];
-            row.appendChild(openPrice);
-            const highPrice = document.createElement('td');
-            highPrice.textContent = item["highPrice"];
-            row.appendChild(highPrice);
-            const lowPrice = document.createElement('td');
-            lowPrice.textContent = item["lowPrice"];
-            row.appendChild(lowPrice);
-            const volume = document.createElement('td');
-            volume.textContent = item["volume"];
-            row.appendChild(volume);
-            const openTime = document.createElement('td');
-            let openDate = new Date(Number(item["openTime"]));
-            openTime.textContent = openDate.toLocaleString();
-            row.appendChild(openTime);
-            const closeTime = document.createElement('td');
-            let closeDate = new Date(Number(item["closeTime"]));
-            closeTime.textContent = closeDate.toLocaleString();
-            row.appendChild(closeTime);
-            // Append row to the table body
-            tableBody.appendChild(row);
+
         }
     });
 }
 
 
-
-// Call getPrice to load data when the page is loaded
-// window.onload = getPrice;
-
 // Stop Alarm Sound
-let soundAlarm;
-function stopAlarm(){
+let soundAlarm = false;
+function stopAlarm() {
     if (soundAlarm) {
-      soundAlarm = false;
-      document.getElementById('stopAndStart').innerText = 'Start';
+        soundAlarm = false;
+        document.getElementById('stopAndStart').innerText = 'Start Alarm Sound';
     } else {
         soundAlarm = true;
-        document.getElementById('stopAndStart').innerText = 'Stop';
+        document.getElementById('stopAndStart').innerText = 'Stop Alarm Sound';
     }
 }
 
 
-
-
+// Call getPrice to load data when the page is loaded
+// window.onload = getPrice;
